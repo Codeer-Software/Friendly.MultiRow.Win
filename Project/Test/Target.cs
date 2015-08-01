abb5d68450beabda162136cbf2baef5a43d0381b
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-08-01 15:31:08 +0900

    作成

---
+++ Project/Test/Target.cs
@@ -0,0 +1,14 @@
+﻿namespace Test
+{
+    static class Target
+    {
+        internal static string Path
+        {
+#if DEBUG
+            get { return System.IO.Path.GetFullPath("../../../Target/bin/Debug/Target.exe"); }
+#else
+            get { return System.IO.Path.GetFullPath("../../../Target/bin/Release/Target.exe"); }
+#endif
+        }
+    }
+}
---
+++ Project/Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using System.Diagnostics;
+using Codeer.Friendly.Windows.NativeStandardControls;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        WindowsAppFriend _app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _app = new WindowsAppFriend(Process.Start(Target.Path));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Process.GetProcessById(_app.ProcessId).Kill();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
